55d896c276c6f93dd34b649fc444141ce6dac789
Joshua Evans <jevans@c02cg2fsmd6q.desire2learn.d2l>
2020-05-22 10:46:30 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: 'us-east-2',
+        BUCKET: 'notes-file-attachment-uploads'
+    },
+    apiGateway: {
+        REGION: 'us-east-2',
+        URL: 'https://35wqap6qg3.execute-api.us-east-2.amazonaws.com/prod'
+    },
+    cognito: {
+        REGION: 'us-east-2',
+        USER_POOL_ID: 'us-east-2_BSDG7UbTp',
+        APP_CLIENT_ID: '6c7pamb048d31rl8ustvkc5g12',
+        IDENTITY_POOL_ID: 'us-east-2:2482289f-e286-4b51-95e1-0884d28cb093'
+    }
+};
